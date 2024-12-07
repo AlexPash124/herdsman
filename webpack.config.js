7b969a4860767555ed011629f1b74efae77c3e8d
Oleksandr Pashkivskyi <o.paskivskij@bossgs.com>
2024-12-07 20:43:44 +0200

    Upd package

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin'); // Імпортуємо плагін
+
 
 module.exports = {
     mode: 'development',
@@ -8,6 +10,7 @@ module.exports = {
         filename: 'bundle.js', // Ім'я згенерованого JS файлу
         path: path.resolve(__dirname, 'dist'), // Куди зберігати файли
         clean: true, // Очищує dist перед бандлом
+        assetModuleFilename: 'assets/[name][ext][query]'
     },
     resolve: {
         extensions: ['.ts', '.js'],
@@ -19,11 +22,24 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.(png|jpe?g|gif|svg|webp)$/,
+                type: 'asset/',
+            },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html', // Використовує ваш HTML файл як шаблон
+            patterns: [
+                { from: path.resolve(__dirname, 'src/assets'), to: 'assets' } // Копіюємо папку assets у dist
+            ],
+        }),
+
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, 'src/assets'), to: 'assets' } // Копіюємо папку assets у dist
+            ],
         }),
     ],
     devServer: {
